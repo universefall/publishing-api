3b82ab402cb729c3cb22f551521cdaa7407919e7
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2015-08-21 14:51:02 +0000

    Add ContentItem model.
    Based closely on model from content-store. Adds version and state
    fields, also separates base_path from ID.
    
    (Note we can't use content_id as ID either, because different versions
    of a doc will have the same content_id.)

---
+++ app/models/content_item.rb
@@ -0,0 +1,28 @@
+class ContentItem
+  include Mongoid::Document
+  include Mongoid::Timestamps::Updated
+
+  field :content_id, :type => String, :overwrite => true, default: ->{ SecureRandom.uuid }
+  field :version, :type => Integer, default: 1
+  field :base_path, :type => String
+  field :state, :type => String, default: "draft"
+  field :title, :type => String
+  field :description, :type => String
+  field :format, :type => String
+  field :locale, :type => String, :default => I18n.default_locale.to_s
+  field :need_ids, :type => Array, :default => []
+  field :public_updated_at, :type => DateTime
+  field :details, :type => Hash, :default => {}
+  field :publishing_app, :type => String
+  field :rendering_app, :type => String
+  field :routes, :type => Array, :default => []
+  field :redirects, :type => Array, :default => []
+  field :links, :type => Hash, :default => {}
+  field :access_limited, :type => Hash, :default => {}
+  attr_accessor :update_type
+
+  validates :base_path, presence: true #, absolute_path: true
+  validates :content_id, presence: true # uuid: true, allow_nil: true
+  validates :format, :publishing_app, presence: true
+  validates :state, inclusion: { in: ["draft", "published", "submitted", "withdrawn"] }
+end
