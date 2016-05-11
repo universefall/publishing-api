a71805a897014f550009d46eaeaccd7d71c6537b
Ben Hyland <ben.hyland@digital.cabinet-office.gov.uk>
2016-05-11 17:07:03 +0100

    more test structure, generating test uuids

--- spec/requests/latest_content_index_spec.rb
+++ spec/requests/latest_content_index_spec.rb
@@ -2,17 +2,14 @@ require "rails_helper"
 
 RSpec.describe "GET /v2/latestcontent", type: :request do
 
-  # create_content_item(:item1)
-  # create_content_item(:item2)
-
   context "input parameter validation" do
 
     it "defaults to the first batch when last_seen_content_id is not provided" do
 
+      create_content_items('0001', '0002', '0003')
+
       expected_result = [
-          # todo...
-          hash_including(title: "Policy 1"),
-          hash_including(title: "Policy 2"),
+          hash_including(content_id: make_test_id('0001')),
       ]
 
       get "/v2/latestcontent", count: 1
@@ -23,16 +20,12 @@ RSpec.describe "GET /v2/latestcontent", type: :request do
 
     it "defaults to a reasonable batch size when count is not provided" do
 
-      expected_result = [
-          # todo...
-          hash_including(title: "Policy 1"),
-          hash_including(title: "Policy 2"),
-      ]
+      create_n_content_items(11)
 
       get "/v2/latestcontent", last_seen_content_id: '00000000-0000-0000-0000-000000000000'
 
       expect(response.status).to eq(200)
-      expect(results(response)).to have_size(10)
+      expect(results(response).size).to eq(10)
     end
 
     it "422s when count is not parsed as an integer" do
@@ -61,13 +54,14 @@ RSpec.describe "GET /v2/latestcontent", type: :request do
 
     it "delivers all content items matching the content_ids contained in the batch" do
 
+      create_content_items('0001', '0002', '0003', '0005', '0007', '0008')
+
       expected_result = [
-          # todo...
-          hash_including(title: "Policy 1"),
-          hash_including(title: "Policy 2"),
+          hash_including(content_id: make_test_id('0005')),
+          hash_including(content_id: make_test_id('0007')),
       ]
 
-      get "/v2/latestcontent", last_seen_content_id: '00000000-1111-0000-0000-000000000000', count: 2
+      get "/v2/latestcontent", last_seen_content_id: make_test_id('0003'), count: 2
 
       expect(response.status).to eq(200)
       expect(results(response)).to match_array(expected_result)
@@ -75,12 +69,13 @@ RSpec.describe "GET /v2/latestcontent", type: :request do
 
     it "delivers a partial batch when reaching the final content_id" do
 
+      create_content_items('0001', '0002', '0003', '0005', '0007', '0008')
+
       expected_result = [
-          # todo...
-          hash_including(title: "Policy 1"),
+          hash_including(content_id: make_test_id('0008')),
       ]
 
-      get "/v2/latestcontent", last_seen_content_id: '00000000-1111-0000-0000-000000000000', count: 2
+      get "/v2/latestcontent", last_seen_content_id: make_test_id('0007'), count: 2
 
       expect(response.status).to eq(200)
       expect(results(response)).to match_array(expected_result)
@@ -88,9 +83,11 @@ RSpec.describe "GET /v2/latestcontent", type: :request do
 
     it "delivers an empty batch when reading beyond the final content_id" do
 
+      create_content_items('0001', '0002', '0003', '0005', '0007', '0008')
+
       expected_result = []
 
-      get "/v2/latestcontent", last_seen_content_id: '00000000-1111-0000-0000-000000000000', count: 2
+      get "/v2/latestcontent", last_seen_content_id: make_test_id('0008'), count: 2
 
       expect(response.status).to eq(200)
       expect(results(response)).to match_array(expected_result)
@@ -104,15 +101,13 @@ RSpec.describe "GET /v2/latestcontent", type: :request do
 
       expected_result = [
           # todo...
-          hash_including(title: "Policy 1"),
-          hash_including(title: "Policy 2"),
       ]
 
       get "/v2/latestcontent", count: 1
 
       expect(response.status).to eq(200)
-      expect(results(response)).to match_array(expected_result)
     end
+
   end
 
 private
@@ -121,13 +116,25 @@ private
     JSON.parse(response.body, symbolize_names: true)[:results]
   end
 
-  def create_content_item(name, state = 'draft', format = 'policy')
-    let!(name) {
-      FactoryGirl.create(:content_item,
-                         state: state,
-                         format: format,
-                         title: "Test Policy Title",
-      )
-    }
+  def create_n_content_items(count)
+    create_content_items(*((1..count).to_a.map { |i| i.to_s.rjust(4, '0') }))
+  end
+
+  def create_content_items(*ids)
+    ids.each { |id| create_content_item(id) }
+  end
+
+  def create_content_item(id)
+    test_content_id = make_test_id(id)
+    FactoryGirl.create(:content_item,
+                       content_id: test_content_id,
+                       base_path: "/#{test_content_id}",
+    )
+  end
+
+  # requires a 4-digit id string
+  def make_test_id(id)
+    # arbitrary type-4 uuid with easier ordering for readable tests
+    uuid = "7c88c837-b76c-496d-b112-9a5a50b0#{id}"
   end
 end
