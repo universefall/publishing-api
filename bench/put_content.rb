0892f8f419c2a5d1c24c2f6a38968aa9017cd7c8
Paul Bowsher <paul.bowsher@gmail.com>
2016-05-24 23:06:59 +0100

    Add benchmark

---
+++ bench/put_content.rb
@@ -0,0 +1,53 @@
+# /usr/bin/env ruby
+
+require ::File.expand_path('../../config/environment', __FILE__)
+require 'benchmark'
+require 'securerandom'
+
+require 'faker'
+
+ContentItem.where("(routes->>0)::json->>'path' LIKE '/performance-testing/%'").each do |ci|
+  LinkSet.where(content_id: ci.content_id).destroy_all
+  ci.destroy
+end
+
+content_items = 100.times.map do
+  title = Faker::Company.catch_phrase
+  {
+    content_id: SecureRandom.uuid,
+    format: "placeholder",
+    schema_name: "placeholder",
+    document_type: "placeholder",
+    title: title,
+    base_path: "/performance-testing/#{title.parameterize}",
+    description: Faker::Lorem.paragraph,
+    public_updated_at: Time.now.iso8601,
+    locale: "en",
+    routes: [
+      {path: "/performance-testing/#{title.parameterize}", type: "exact"}
+    ],
+    redirects: [],
+    publishing_app: "test",
+    rendering_app: "test",
+    details: {
+      body: "<p>#{Faker::Lorem.paragraph}</p>"
+    },
+    phase: 'live',
+    need_ids: []
+  }
+end
+
+$queries = 0
+ActiveSupport::Notifications.subscribe "sql.active_record" do |name, started, finished, unique_id, data|
+  $queries += 1
+end
+
+puts Benchmark.measure {
+  content_items.each do |item|
+    Commands::V2::PutContent.call(item)
+    print "."
+  end
+  puts ""
+}
+
+puts "#{$queries} SQL queries"
