a1635a25b962fc9e231fb8e2037b6d8e2541830a
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2016-09-12 16:07:23 +0000

    Task for comparing govspeak renderings

---
+++ lib/data_hygiene/govspeak_compare.rb
@@ -0,0 +1,78 @@
+require "diffy"
+
+module DataHygiene
+  class GovspeakCompare
+    attr_reader :content_item
+
+    def initialize(content_item)
+      @content_item = content_item
+    end
+
+    def published_html
+      @published_html ||= format_published_html
+    end
+
+    def generated_html
+      @generated_html ||= html_from_details(
+        Presenters::DetailsPresenter.new(content_item.details_for_govspeak_conversion).details
+      )
+    end
+
+    def diffs
+      @diffs ||= calculate_diffs
+    end
+
+    def same_html?
+      HashDiff.diff(published_html, generated_html) == []
+    end
+
+    def pretty_much_same_html?
+      return true if same_html?
+      diffs.all? { |_, diff| diff == [] }
+    end
+
+  private
+
+    def calculate_diffs
+      keys = (published_html.keys + generated_html.keys).uniq.sort
+      keys.each_with_object({}) do |key, memo|
+        memo[key] = html_diff(published_html[key], generated_html[key])
+      end
+    end
+
+    def html_diff(old_html, new_html)
+      diff = Diffy::Diff.new(old_html, new_html, context: 1)
+      # remove matches and empty lines
+      diff = diff.reject { |s| s.match(/^ /) || s.match(/^(\+|-)$/) }
+      # we don't include items where there is an identical one matching it
+      # without spaces - this could mean we miss ordering issues but they are
+      # unlikely to occur here
+      without_spaces = diff.map { |s| s.gsub(/\s+/, "") }
+      without_spaces_and_rel_external = without_spaces.map { |s| s.gsub(/rel="external"/, "") }
+      diff.reject do |s|
+        check = (s[0] == "+" ? "-" : "+") + s[1..-1].gsub(/\s+/, "")
+        next true if without_spaces.include?(check)
+        # we have issues that some attachments are published with rel="external"
+        without_spaces_and_rel_external.include?(check.gsub(/rel="external"/, ""))
+      end
+    end
+
+    def format_published_html
+      html_details = html_from_details(content_item.details)
+      html_details.each_with_object({}) do |(key, value), memo|
+        # pushed through nokogiri to catch minor html differences (<br /> -> <br>, unicode characters)
+        memo[key] = Nokogiri::HTML.fragment(value).to_html
+      end
+    end
+
+    def html_from_details(details)
+      details.each_with_object({}) do |(key, value), memo|
+        wrapped = Array.wrap(value)
+        html = wrapped.find do |item|
+          item.is_a?(Hash) && item[:content_type] == "text/html"
+        end
+        memo[key] = html[:content] if html && html[:content]
+      end
+    end
+  end
+end
---
+++ lib/tasks/govspeak.rake
@@ -0,0 +1,33 @@
+namespace :govspeak do
+  task :compare, [:publishing_app, :limit, :offset, :order] => :environment do |_, args|
+    args.with_defaults(order: "content_items.id ASC")
+    scope = State.joins(:content_item)
+    scope = scope.where(content_items: { publishing_app: args[:publishing_app] }) if args[:publishing_app].present?
+    scope = scope.where(name: %w(published unpublished draft))
+    scope = scope.limit(args[:limit]) if args[:limit].present?
+    scope = scope.offset(args[:offset]) if args[:offset].present?
+    scope = scope.order(args[:order])
+    total = scope.count
+    same_html = 0
+    trivial_differences = 0
+    scope.each do |state|
+      content_item = state.content_item
+      comparer = DataHygiene::GovspeakCompare.new(content_item)
+      same_html += 1 if comparer.same_html?
+      trivial_differences += 1 if !comparer.same_html? && comparer.pretty_much_same_html?
+      next if comparer.pretty_much_same_html?
+      puts "Content Item #{content_item.id} #{content_item.content_id}"
+      comparer.diffs.each do |field, diff|
+        next if diff == []
+        puts field
+        diff.each do |item|
+          print item.red if item[0] == "-"
+          print item.green if item[0] == "+"
+        end
+      end
+    end
+    puts "Same HTML: #{same_html}/#{total}"
+    puts "Trivial Differences: #{trivial_differences}/#{total}"
+    puts "Other differences: #{total - (same_html + trivial_differences)}/#{total}"
+  end
+end
